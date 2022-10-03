878f6dc2b0b25a06e683884bf4ac28cbe99fddfd
Iqbal Yudiar <iqbal.yudiar@dana.id>
2022-10-03 21:44:01 +0700

    feat: add restaurants list on home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,14 @@
 import React, { useContext, useCallback } from "react";
 import { useFocusEffect } from "@react-navigation/native";
-import { StyleSheet, Text, View } from "react-native";
-import { Button } from "@rneui/base";
+import { StyleSheet, FlatList, View, TouchableOpacity } from "react-native";
+import { ListItem, Avatar } from "@rneui/base";
 import { Context as RestaurantContext } from "../context/RestaurantContext";
 
 const HomeScreen = ({ navigation }) => {
-  const { getRestaurants } = useContext(RestaurantContext);
+  const {
+    state: { restaurants },
+    getRestaurants,
+  } = useContext(RestaurantContext);
 
   useFocusEffect(
     useCallback(() => {
@@ -15,8 +18,24 @@ const HomeScreen = ({ navigation }) => {
 
   return (
     <View>
-      <Text>HomeScreen</Text>
-      <Button title="Go to cart" onPress={() => navigation.navigate("Cart")} />
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={restaurants}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <ListItem bottomDivider>
+              <Avatar
+                title={item.name[0]}
+                source={item.logo && { uri: item.logo }}
+              />
+              <ListItem.Content>
+                <ListItem.Title>{item.name}</ListItem.Title>
+              </ListItem.Content>
+              <ListItem.Chevron />
+            </ListItem>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
