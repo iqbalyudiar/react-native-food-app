64f9cddf2a5580df284582a56ed9ed9e5fd14ece
Iqbal Yudiar <iqbal.yudiar@dana.id>
2022-10-03 21:31:05 +0700

    feat: add context to home screen

--- src/context/RestaurantContext.js
+++ src/context/RestaurantContext.js
@@ -1,11 +1,15 @@
 import React, { useReducer } from "react";
+import foodApi from "../api/food";
+import { GET_RESTAURANTS } from "./types";
 
 const initialState = {
-  restaurant: [],
+  restaurants: [],
 };
 
 const restaurantReducer = (state, action) => {
   switch (action.type) {
+    case GET_RESTAURANTS:
+      return { ...state, restaurants: action.payload };
     default:
       return state;
   }
@@ -16,9 +20,18 @@ export const Context = React.createContext();
 export const Provider = ({ children }) => {
   const [state, dispatch] = useReducer(restaurantReducer, initialState);
 
-  const getRestaurants = () => {};
+  const getRestaurants = async () => {
+    try {
+      const response = await foodApi.get("/restaurants");
+      dispatch({ type: GET_RESTAURANTS, payload: response.data });
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
 
-  const value = { state, getRestaurants };
+  const actions = { getRestaurants };
+
+  const value = { state, ...actions };
 
   return <Context.Provider value={value}>{children}</Context.Provider>;
 };
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,18 @@
+import React, { useContext, useCallback } from "react";
+import { useFocusEffect } from "@react-navigation/native";
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "@rneui/base";
-import React from "react";
+import { Context as RestaurantContext } from "../context/RestaurantContext";
 
 const HomeScreen = ({ navigation }) => {
+  const { getRestaurants } = useContext(RestaurantContext);
+
+  useFocusEffect(
+    useCallback(() => {
+      getRestaurants();
+    }, [])
+  );
+
   return (
     <View>
       <Text>HomeScreen</Text>
