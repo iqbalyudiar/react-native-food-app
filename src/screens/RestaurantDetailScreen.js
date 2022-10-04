2c1330d8450b3b0e27db303d81690e3282e210b5
Iqbal Yudiar <iqbal.yudiar@dana.id>
2022-10-04 20:05:12 +0700

    feat: add restaurant detail screen implementation

--- src/screens/RestaurantDetailScreen.js
+++ src/screens/RestaurantDetailScreen.js
@@ -1,14 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useContext } from "react";
+import { StyleSheet, View } from "react-native";
+import { Context as RestaurantContext } from "../context/RestaurantContext";
+import { Card, Text, Button } from "@rneui/base";
 
-const RestaurantDetailScreen = ({route}) => {
+const RestaurantDetailScreen = ({ route }) => {
+  const {
+    state: { restaurants },
+  } = useContext(RestaurantContext);
+
+  const { id } = route.params;
+
+  const currentRestaurant = restaurants.find((resto) => resto.id === id);
+
   return (
     <View>
-      <Text>RestaurantDetailScreen</Text>
+      {currentRestaurant.foods.map((food) => (
+        <Card key={food.id}>
+          <View style={styles.foodCard}>
+            <Text h3>{food.name}</Text>
+            <View>
+              <Text h4 style={styles.priceText}>
+                Rp{food.price}
+              </Text>
+              <View style={styles.actionButtonGroup}>
+                <Button
+                  buttonStyle={styles.actionButton}
+                  title="-"
+                  color="error"
+                  size="md"
+                  radius="md"
+                />
+                <Button
+                  buttonStyle={styles.actionButton}
+                  title="+"
+                  color="success"
+                  size="md"
+                  radius="md"
+                />
+              </View>
+            </View>
+          </View>
+        </Card>
+      ))}
     </View>
-  )
-}
+  );
+};
 
-export default RestaurantDetailScreen
+export default RestaurantDetailScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  foodCard: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  priceText: {
+    marginBottom: 10,
+  },
+  actionButtonGroup: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+  },
+  actionButton: {
+    width: 35,
+    marginHorizontal: 2,
+  },
+});
