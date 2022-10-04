c06f7565f436c8942f698d400b90b4f861016d20
Iqbal Yudiar <iqbal.yudiar@dana.id>
2022-10-04 21:41:05 +0700

    feat: update add to cart conditional on reducer

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -6,31 +6,23 @@ const initialState = {
 };
 
 const cartReducer = (state, action) => {
+  const found = state.carts.some((cart) => cart.id === action.payload.id);
   switch (action.type) {
     case ADD_TO_CART:
-      if (state.carts.length === 0) {
-        return { ...state, carts: [{ ...action.payload, quantity: 1 }] };
-      } else {
-        const index = state.carts.findIndex(
-          (cart) => cart.id === action.payload.id
+      if (found) {
+        const data = state.carts.map((cart) =>
+          cart.id === action.payload.id
+            ? { ...cart, quantity: cart.quantity + 1 }
+            : cart
         );
-
-        if (index !== -1) {
-          const data = state.carts.map((cart) =>
-            cart.id === action.payload.id
-              ? { ...cart, quantity: cart.quantity + 1 }
-              : cart
-          );
-          return { ...state, carts: data };
-        }
-        return {
-          ...state,
-          carts: [...state.carts, ...[{ ...action.payload, quantity: 1 }]],
-        };
+        return { ...state, carts: data };
       }
-    case REMOVE_FROM_CART:
-      const found = state.carts.some((cart) => cart.id === action.payload.id);
 
+      return {
+        ...state,
+        carts: [...state.carts, ...[{ ...action.payload, quantity: 1 }]],
+      };
+    case REMOVE_FROM_CART:
       if (found) {
         const newCarts = state.carts
           .map((cart) =>
