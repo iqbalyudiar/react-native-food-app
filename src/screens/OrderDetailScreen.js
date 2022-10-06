c042849d8f8ee7807202293cc316044bf6becf55
Iqbal Yudiar <iqbal.yudiar@dana.id>
2022-10-06 22:55:11 +0700

    feat: add order detail ui

--- src/screens/OrderDetailScreen.js
+++ src/screens/OrderDetailScreen.js
@@ -1,14 +1,71 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useContext } from "react";
+import { StyleSheet, View } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { Context as OrderContext } from "../context/OrderContext";
+import { AntDesign } from "@expo/vector-icons";
+import { Text } from "@rneui/base";
+import OrderSummary from "../components/OrderSummary";
 
 const OrderDetailScreen = () => {
+  const {
+    state: { orders },
+  } = useContext(OrderContext);
+  const route = useRoute();
+  const selectedOrder = orders.find((order) => order.id === route.params.id);
+  const { items, restaurant, totalPrice, status } = selectedOrder;
+
+  const CompletedStatus = () => {
+    return (
+      <>
+        <AntDesign
+          name="checkcircleo"
+          size={50}
+          color="black"
+          style={styles.text}
+        />
+        <Text h3 style={styles.text}>
+          Your order is completed
+        </Text>
+      </>
+    );
+  };
+
+  const InprogressStatus = () => {
+    return (
+      <>
+        <AntDesign
+          name="clockcircleo"
+          size={50}
+          color="black"
+          style={styles.text}
+        />
+        <Text h3 style={styles.text}>
+          Your order is in progress
+        </Text>
+      </>
+    );
+  };
   return (
     <View>
-      <Text>OrderDetailScreen</Text>
+      {selectedOrder.status === "completed" ? (
+        <CompletedStatus />
+      ) : (
+        <InprogressStatus />
+      )}
+      <OrderSummary
+        data={items}
+        restaurant={restaurant}
+        totalPrice={totalPrice}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default OrderDetailScreen
+export default OrderDetailScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  text: {
+    textAlign: "center",
+    marginTop: 15,
+  },
+});
